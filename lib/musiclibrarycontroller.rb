46461d97d2986ab1f475d3ae84ba3cd5ba8a73b5
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-13 18:42:12 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -45,6 +45,7 @@ class MusicLibraryController
   def list_songs
     Song.all.sort{|a,b| a.name <=> b.name}.each_with_index(1) do |song,i|
       puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre}"
+    end
   end
   
 end
