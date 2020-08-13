94fa4ac484c6b61b1f86b47a0e775328552281ce
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-13 18:54:52 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -50,9 +50,7 @@ class MusicLibraryController
   
   def list_artists
     artists = Artist.all.sort {|a,b| a.name <=> b.name}
-    artists.each.with_index(1) do |artist,i|
-      puts "#{i}. #{artist.name}"
-    end
+    artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
   end
   
 end
