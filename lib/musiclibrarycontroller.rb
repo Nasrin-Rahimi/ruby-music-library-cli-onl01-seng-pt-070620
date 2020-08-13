204ef8f32446993c5a0549a345eee1dbc846ff3a
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-13 18:48:19 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each_with_index(1) do |song, i|
-      puts "eywer"
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
   
